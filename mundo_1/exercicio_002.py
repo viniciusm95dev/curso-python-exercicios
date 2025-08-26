7bde5a1469ebd74b2282ca2731cc8a8dc64fbaa4
viniciusm95dev <viniciusm95dev@gmail.com>
2025-08-26 14:41:40 -0300

    feat: Mundo 1 - Exercício 002 - Curso Python

---
+++ mundo_1/exercicio_002.py
@@ -0,0 +1,11 @@
+"""Exercício 002
+Curso de Python 3 - Gustavo Guanabara
+"""
+
+#Faça um programa que leia o nome de uma pessoa e mostre uma msg de boas-vindas.
+
+
+nome = input('Digite seu nome: ')
+print('É um prazer te conhecer {}.'.format(nome))
+
+
