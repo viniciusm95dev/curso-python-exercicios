d41b77efde24708fc86d79051906266ef0e93fc4
viniciusm95dev <viniciusm95dev@gmail.com>
2025-08-26 14:41:51 -0300

    feat: Mundo 1 - Exercício 003 - Curso Python

---
+++ mundo_1/exercicio_003.py
@@ -0,0 +1,12 @@
+"""Exercício 003
+Curso de Python 3 - Gustavo Guanabara
+"""
+#Crie um programa que leia dois numeros e mostre a soma entre eles.
+
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+
+resultado = n1 + n2
+
+print('A soma entre {} e {} é igua a: {}'.format(n1, n2, resultado))
+
